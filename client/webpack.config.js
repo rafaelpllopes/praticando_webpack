8b2ff8091836427f7c5ec0c8297bd573a0343349
rafaelpllopes <rafael.pllopes@gmail.com>
2018-07-19 15:03:29 -0300

    Implementando as boas praticas, otimizando o build, separando os codigos das bibliotecas em um vendor com webpack.optimize.CommonsChunkPlugin.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,9 +11,15 @@ plugins.push(new extractTextPlugin('styles.css'));
 plugins.push(new webpack.ProvidePlugin({
     '$': 'jquery/dist/jquery.js',
     'jQuery': 'jquery/dist/jquery.js'
-}))
+}));
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
 
 if(process.env.NODE_ENV == 'production') {
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -28,7 +34,10 @@ if(process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
