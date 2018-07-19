101e8060127a63bebc5d686bea449f7e6d974b98
rafaelpllopes <rafael.pllopes@gmail.com>
2018-07-19 16:02:38 -0300

    Alterar o endereço da API de produção com o DefinePlugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,7 +30,12 @@ plugins.push(new webpack.optimize.CommonsChunkPlugin({
     filename: 'vendor.bundle.js'
 }));
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if(process.env.NODE_ENV == 'production') {
+
+    SERVICE_URL = JSON.stringify('http://teste.teste.api');
+
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
@@ -44,6 +49,8 @@ if(process.env.NODE_ENV == 'production') {
         canPrint: true
     }));
 }
+
+plugins.push(new webpack.DefinePlugin({SERVICE_URL}));
 
 module.exports = {
     entry: {
